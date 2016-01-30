9564ff510d144db75f717df76d9fd5d53ebec81e
Ernst Vorsteveld <ernst.vorsteveld@iwelcome.com>
2016-01-30 11:32:31 +0100

    started to add javascript test for controller

--- src/test/resources/static/spec/view/form/from.controller.spec.js
+++ src/test/resources/static/spec/view/form/from.controller.spec.js
@@ -1,9 +1,37 @@
 describe("Form controller test.", function () {
 
+    var controller;
+    var $httpBackend;
+    var $scope;
 
+    var expectedUrl = '/persons';
+    var responseData = [ {'test': 'val1'}];
 
-  it("Should load all.", function () {
-      expect(true).toBe(true);
-  });
+    beforeEach(function () {
+        module('app');
+    });
+
+    beforeEach(inject(function ($controller, $rootScope, _$httpBackend_) {
+         controller = $controller('formController', {
+             $scope : $rootScope.$new()
+         });
+         $httpBackend = _$httpBackend_;
+     }));
+
+     afterEach(function() {
+        $httpBackend.verifyNoOutstandingExpectation();
+        $httpBackend.verifyNoOutstandingRequest();
+     });
+
+    it("Should have default value for username.", function () {
+        expect(controller.username).toBe('username1');
+    });
+
+    it("Should load all.", function () {
+        $httpBackend.expect("GET", expectedUrl).respond(200, responseData);
+        controller.loadAll();
+        $httpBackend.flush();
+        expect(controller.persons.test).toBe(responseData.test);
+    });
 
 });
