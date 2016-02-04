dfb936408d9f4cd4fcd5987ab7a947b170946bb5
Ernst Vorsteveld <ernst.vorsteveld@iwelcome.com>
2016-02-04 18:13:17 +0100

    minor refactoring: - text as one paragraph, deleted new lines - moved package to the correct place - import organisation

--- src/main/java/nl/vorstdev/example/resource/Application.java
+++ src/main/java/nl/vorstdev/example/resource/Application.java
@@ -1,21 +1,14 @@
 package nl.vorstdev.example.resource;
 
-import com.google.common.collect.Lists;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import springfox.documentation.builders.PathSelectors;
-import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.builders.ResponseMessageBuilder;
-import springfox.documentation.schema.ModelRef;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 /**
  * Created by ernstvorsteveld on 16/01/16.
--- src/main/java/nl/vorstdev/example/resource/actuator/ShowEndpoints.java
+++ src/main/java/nl/vorstdev/example/resource/actuator/ShowEndpoints.java
@@ -1,16 +1,16 @@
 package nl.vorstdev.example.resource.actuator;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.actuate.endpoint.AbstractEndpoint;
 import org.springframework.boot.actuate.endpoint.Endpoint;
 import org.springframework.stereotype.Component;
 
-@Component
-public class ShowEndpoints extends AbstractEndpoint<List<Endpoint>>{
+import java.util.List;
 
-  private List<Endpoint> endpoints;
+@Component
+public class ShowEndpoints extends AbstractEndpoint<List<Endpoint>> {
+
+    private List<Endpoint> endpoints;
 
     @Autowired
     public ShowEndpoints(List<Endpoint> endpoints) {
