fa6012e3cc2bc969d43adc852982895f11667ac8
Ernst Vorsteveld <ernst.vorsteveld@iwelcome.com>
2016-02-15 11:45:28 +0100

    added jenkins job definition

---
+++ jobs/job-test.groovy
@@ -0,0 +1,29 @@
+String basePath = 'Example-resource'
+
+folder(basePath) {
+    description 'This example shows basic folder/job creation.'
+}
+
+freeStyleJob("$basePath/clean-and-compile") {
+    description {
+        'Clean and compile the project.'
+    }
+    steps {
+        grails {
+            useWrapper true
+            targets(['clean', 'classes'])
+        }
+    }
+}
+
+job("$basePath/test") {
+    description {
+        'Test the project.'
+    }
+    steps {
+        grails {
+            useWrapper true
+            targets(['test'])
+        }
+    }
+}
