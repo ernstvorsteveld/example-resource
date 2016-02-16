83bd4ffdb2dd9acbbb3b731539a95cd6aae5e5f7
Ernst Vorsteveld <ernst.vorsteveld@iwelcome.com>
2016-02-16 12:55:58 +0100

    pmd check added to gradle
    deleted for pmd (check failes otherwise)
    added example class to show the result of pmd check

---
+++ src/main/java/nl/vorstdev/example/resource/pmd/PmdExample.java
@@ -0,0 +1,11 @@
+package nl.vorstdev.example.resource.pmd;
+
+/**
+ * Created by ernstvorsteveld on 16/02/16.
+ */
+public class PmdExample {
+
+    public void hasPmdError() {
+        if(true) {}
+    }
+}
